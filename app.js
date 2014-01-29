eaef40156114bb254f7defaf672a8398463005a6
James Muranga <jmured@gmail.com>
2014-01-29 17:37:41 +0300

    [MJ] Added protocol to the config for the remote server

--- app.js
+++ app.js
@@ -6,53 +6,53 @@ var path = require('path');
 var config_file = path.join(__dirname, 'config.json');
 var config = require('./config.js');
 var getXml= function(req){
-	
-	var parseString = require('xml2js').parseString;
+
+    var parseString = require('xml2js').parseString;
 
     config.init(config_file, function(resp) {
         if (resp != 0) {
             console.log('Could not load config file.');
-         return;
+            return;
         }
-    var options = config.url;
+        var options = config.url;
 
-	http.get(options, function(res) {
-	    console.log('STATUS: ' + res.statusCode);
-	    console.log('HEADERS: ' + JSON.stringify(res.headers));
-	    res.setEncoding('utf8');
-		var pageData = "";
-		res.setEncoding('utf8');
-		res.on('data', function (chunk) {
-		      pageData += chunk;
-		 });
-		 res.on('end', function(){
-	  			parseString(pageData,{trim: true}, function (err, result) {
-					// console.log(result.Projects);
-					result.Projects.Project.forEach(function(item) {
-						console.log(item);
-					})
-					
-					req.io.emit('talk', {
-				        message: result
-				    });
-				
-	  		});
-		  });
-   
+        http.get(options, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            var pageData = "";
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                pageData += chunk;
+            });
+            res.on('end', function(){
+                parseString(pageData,{trim: true}, function (err, result) {
+                    // console.log(result.Projects);
+                    result.Projects.Project.forEach(function(item) {
+                        console.log(item);
+                    })
 
-	    }).on('error', function(e) {
-	        console.log('ERROR: ' + e.message);
-	    });
+                    req.io.emit('talk', {
+                        message: result
+                    });
+
+                });
+            });
+
+
+        }).on('error', function(e) {
+            console.log('ERROR: ' + e.message);
+        });
 
     });    
 
-	}
+}
 
 var express = require('express.io')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+, routes = require('./routes')
+, user = require('./routes/user')
+, http = require(config.url.protocol)
+, path = require('path');
 
 var app = express();
 
@@ -71,22 +71,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 
 app.io.route('tray', function(req) {
-    
-	getXml(req);
-	setInterval(function(){ 
-		getXml(req);
-		
-	 }, 10000);
+
+    getXml(req);
+    setInterval(function(){ 
+        getXml(req);
+
+    }, 10000);
 });
- 
+
 
 app.server.listen(app.get('port'), function(){
-   console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
 
